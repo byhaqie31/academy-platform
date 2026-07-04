ff645fbc219f154ce6dd37ef8b951662ebc984f5
Baihaqie Yusri <byhaqie1455@gmail.com>
2026-07-05 07:38:05 +0800

    feat: add favicon.svg and update nuxt.config for favicon links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,10 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', href: '/favicon.svg' },
+      ],
     },
   },
 })
